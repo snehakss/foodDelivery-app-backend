1a664369ea241fa786be13dd93ce0a47894ec1e5
snehakss <snehakallingal123@gmail.com>
2024-02-08 20:25:04 +0530

    search is added

--- controller/foodRouter.js
+++ controller/foodRouter.js
@@ -17,4 +17,9 @@ router.get("/view",async(req,res)=>{
     res.json(data)
 })
 
+router.post("/search",async(req,res)=>{
+    let data=await foodModel.find(input)
+    res.json(data)
+})
+
 module.exports=router
